916ff2b2d07048bd1bb5442247a41554807fced0
nileshhhhhh <2020s18045@stu.cmb.ac.lk>
2025-06-22 18:44:26 +0530

    changed query.py

--- query.py
+++ query.py
@@ -6,10 +6,10 @@ import os
 embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")  # update if you used another
 
 # Step 2: Load the FAISS vector store
-vectorstore = FAISS.load_local("vector_store", embeddings=embedding_model, index_name="index", allow_dangerous_deserialization=True)
+vectorstore = FAISS.load_local("vector_store/Nilesh Fonseka", embeddings=embedding_model, index_name="index", allow_dangerous_deserialization=True)
 
 # Step 3: Run the query
-query = "Show me email of Chamodi Karunathunge"
+query = "Show me email of Nilesh Fonseka"
 docs = vectorstore.similarity_search(query, k=3)
 
 # Step 4: Print the results
